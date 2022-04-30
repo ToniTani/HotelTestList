d8eb2941c11dbcaee031824d24c1cc4852a60e5a
ToniTani <toni.tani.tt@gmail.com>
2022-05-01 00:36:21 +0300

    Hotel Controller

---
+++ HotelTestList/Controllers/HotelController.cs
@@ -0,0 +1,87 @@
+﻿using AutoMapper;
+using HotelTestList.IRepository;
+using HotelTestList.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HotelTestList.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotelController : ControllerBase
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ILogger<HotelController> _logger;
+        private readonly IMapper _mapper;
+
+        public HotelController(IUnitOfWork unitOfWork, ILogger<HotelController> logger, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _logger = logger;
+            _mapper = mapper;
+        }
+        // GET: api/<HotelController>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetHotels()
+        {
+            try
+            {
+                var hotels = await _unitOfWork.Hotels.GetAll();
+                var results = _mapper.Map<IList<HotelDTO>>(hotels);
+                return Ok(hotels);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"somethin went wrong! in {nameof(GetHotels)}");
+                return BadRequest("Submitted data is invalid");
+
+            }
+        }
+
+        // GET api/<HotelController>/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetHotel(int id)
+        {
+            try
+            {
+                var hotel = await _unitOfWork.Hotels.Get(q => q.Id == id, new List<string> { "Country" });
+                var results = _mapper.Map<HotelDTO>(hotel);
+                return Ok(hotel);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"somethin went wrong! in {nameof(GetHotel)}");
+                return BadRequest("Submitted data is invalid");
+
+            }
+        }
+
+        // POST api/<HotelController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<HotelController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<HotelController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
