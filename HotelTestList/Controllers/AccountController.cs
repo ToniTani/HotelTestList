00f45b607329263fb9a26e8fbba916180389c5df
ToniTani <toni.tani.tt@gmail.com>
2022-05-03 01:08:05 +0300

    User Registration Endpoint Fix

--- HotelTestList/Controllers/AccountController.cs
+++ HotelTestList/Controllers/AccountController.cs
@@ -58,7 +58,7 @@ namespace HotelTestList.Controllers
             {
                 var user = _mapper.Map<ApiUser>(userDTO);
                 user.UserName = userDTO.Email;
-                var result = await _userManager.CreateAsync(user);
+                var result = await _userManager.CreateAsync(user, userDTO.Password);
 
                 if (!result.Succeeded)
                 {
