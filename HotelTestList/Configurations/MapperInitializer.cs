b58026001c68e2e767ad428d0c05898da2eb16cc
ToniTani <toni.tani.tt@gmail.com>
2022-05-03 01:05:04 +0300

    User Registration and Login Endpoint

--- HotelTestList/Configurations/MapperInitializer.cs
+++ HotelTestList/Configurations/MapperInitializer.cs
@@ -16,6 +16,7 @@ namespace HotelTestList.Configurations
             CreateMap<Country, CreateCountryDTO>().ReverseMap();
             CreateMap<Hotel, HotelDTO>().ReverseMap();
             CreateMap<Hotel, CreateHotelDTO>().ReverseMap();
+            CreateMap<ApiUser, UserDTO>().ReverseMap();
         }
     }
 }
---
+++ HotelTestList/Controllers/AccountController.cs
@@ -0,0 +1,126 @@
+﻿using AutoMapper;
+using HotelTestList.Data;
+using HotelTestList.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HotelTestList.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+
+        private readonly UserManager<ApiUser> _userManager;
+        private readonly ILogger<AccountController> _logger;
+        private readonly IMapper _mapper;
+
+        public AccountController(UserManager<ApiUser> userManager, ILogger<AccountController> logger, IMapper mapper)
+        {
+            _userManager = userManager;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+
+        // GET: api/<AccountController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<AccountController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<AccountController>
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register([FromBody] UserDTO userDTO)
+        {
+            _logger.LogInformation($"Registration Attemp for {userDTO.Email} ");
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                var user = _mapper.Map<ApiUser>(userDTO);
+                user.UserName = userDTO.Email;
+                var result = await _userManager.CreateAsync(user);
+
+                if (!result.Succeeded)
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(error.Code, error.Description);
+                    }
+                    return BadRequest(ModelState);
+                }
+
+                return Accepted();
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"somethin went wrong! in {nameof(Register)}");
+                return Problem($"somethin went wrong! in { nameof(Register)}", statusCode: 500);
+              
+            }
+       
+        }
+
+        //[HttpPost]
+        //[Route("login")]
+        //public async Task<IActionResult> Login([FromBody] LoginUserDTO userDTO)
+        //{
+        //    _logger.LogInformation($"Login Attemp for {userDTO.Email} ");
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
+        //    try
+        //    {
+        //        var result = await _signInManager.PasswordSignInAsync(userDTO.Email, userDTO.Password, false, false);
+
+        //        if (!result.Succeeded)
+        //        {
+        //            return Unauthorized(userDTO);
+        //        }
+
+        //        return Accepted();
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        _logger.LogError(ex, $"somethin went wrong! in {nameof(Register)}");
+        //        return Problem($"somethin went wrong! in { nameof(Register)}", statusCode: 500);
+
+        //    }
+
+        //}
+
+        // PUT api/<AccountController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<AccountController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
